f3d9393f75eb4a59a21d4a6b3a37c8d1d157f41f
Abe Gillespie <abe.gillespie@workmax.com>
2019-04-02 12:58:52 -0700

    Update Program.cs

--- Project/Program.cs
+++ Project/Program.cs
@@ -11,3 +11,25 @@ namespace Project
         }
     }
 }
+
+// Server
+// docker run \
+//   --volume=/Users/abe/Documents/drone/data:/data \
+//   --env=DRONE_GITHUB_SERVER=https://github.com \
+//   --env=DRONE_GITHUB_CLIENT_ID=f2a7ba4a901c2e2dd235 \
+//   --env=DRONE_GITHUB_CLIENT_SECRET=fc71bb9346f000d7015c49763bedca7c143939cf \
+//   --env=DRONE_AGENTS_ENABLED=true \
+//   --env=DRONE_RPC_SECRET=257984415c3a09e491ae76f6d837940c \
+//   --env=DRONE_SERVER_HOST=drone.machost \
+//   --env=DRONE_SERVER_PROTO=http \
+//   --env=DRONE_TLS_AUTOCERT=true \
+//   --env=DRONE_LOGS_DEBUG=true \
+//   --publish=80:80 \
+//   --publish=443:443 \
+//   --restart=always \
+//   --detach=true \
+//   --name=drone \
+//   drone/drone:1
+
+// Agent
+// docker run --volume=//./pipe/docker_engine://./pipe/docker_engine --env=DRONE_LOGS_DEBUG=true --env=DRONE_RPC_SERVER_HOST=drone.machost --env=DRONE_SERVER_PROTO=http --env=DRONE_RPC_SECRET=257984415c3a09e491ae76f6d837940c --env=DRONE_RUNNER_CAPACITY=2 --restart=always --detach=true --name=agent drone/agent:windows-1803-amd64
