ba1d972040e1fde317ec3c7fcfff88da0bf0633b
Abe Gillespie <abe.gillespie@workmax.com>
2019-04-03 12:45:40 -0700

    Update Program.cs

--- Project/Program.cs
+++ Project/Program.cs
@@ -20,7 +20,7 @@ namespace Project
 //   --env=DRONE_GITHUB_CLIENT_SECRET=fc71bb9346f000d7015c49763bedca7c143939cf \
 //   --env=DRONE_AGENTS_ENABLED=true \
 //   --env=DRONE_RPC_SECRET=257984415c3a09e491ae76f6d837940c \
-//   --env=DRONE_SERVER_HOST=drone.machost \
+//   --env=DRONE_SERVER_HOST=drone.machost.dev \
 //   --env=DRONE_SERVER_PROTO=http \
 //   --env=DRONE_TLS_AUTOCERT=true \
 //   --env=DRONE_LOGS_DEBUG=true \
@@ -32,4 +32,4 @@ namespace Project
 //   drone/drone:1
 
 // Agent
-// docker run --volume=//./pipe/docker_engine://./pipe/docker_engine --env=DRONE_LOGS_DEBUG=true --env=DRONE_RPC_HOST=drone.machost --env=DRONE_RPC_PROTO=http --env=DRONE_RPC_DEBUG=true --env=DRONE_RPC_SECRET=257984415c3a09e491ae76f6d837940c --env=DRONE_RUNNER_CAPACITY=2 --restart=always --detach=true --name=agent drone/agent:windows-1803-amd64
+// docker run --volume=//./pipe/docker_engine://./pipe/docker_engine --env=DRONE_LOGS_DEBUG=true --env=DRONE_RPC_HOST=fecf4211.ngrok.io --env=DRONE_RPC_PROTO=https --env=DRONE_RPC_DEBUG=true --env=DRONE_RPC_SECRET=257984415c3a09e491ae76f6d837940c --env=DRONE_RUNNER_CAPACITY=2 --restart=always --detach=true --name=agent drone/agent:windows-1803-amd64
